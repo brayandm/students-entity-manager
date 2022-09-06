91c03b1d251989a08a066264b6eedacf1b0dc84e
Brayan Duran <brayan.duran@leaguesofcode.com>
2022-09-06 23:09:15 +0200

    View Button added

--- resources/views/listofstudents.blade.php
+++ resources/views/listofstudents.blade.php
@@ -17,21 +17,25 @@
                 <ul class="list-group">
                     <li class="list-group-item"> <b>Fullname:</b>
 
-                        <a href="{{ route('showstudent', ['id' => $student->id]) }}">{{$student->firstname}} {{$student->lastname}}</a>
+                        {{$student->firstname}} {{$student->lastname}}
                         <br>
                         <b>Email:</b> {{$student->email}}</li>
                 </ul>
 
                 <div class="col-md-3 d-flex justify-content-end">
-                    <form action="{{ route('deletestudent', ['id' => $student->id]) }}" method="POST">
-                        @method('DELETE')
-                        @csrf
-                        <button class="btn btn-danger btn-sm">Delete</button>
+                    <form action="{{ route('showstudent', ['id' => $student->id]) }}">
+                        <button class="btn btn-primary btn-sm">View</button>
                     </form>
                     <h1>&nbsp</h1>
                     <form action="{{ route('editstudent', ['id' => $student->id]) }}">
                         @csrf
                         <button class="btn btn-warning btn-sm">Edit</button>
+                    </form>
+                    <h1>&nbsp</h1>
+                    <form action="{{ route('deletestudent', ['id' => $student->id]) }}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <button class="btn btn-danger btn-sm">Delete</button>
                     </form>
                 </div>
 
